099abfcb5c92e08420e6fc52536a30551ff02320
Parker Pestana <parker@everlane.com>
2018-12-03 22:45:13 -0800

    styles the header a bit and collage break in progress

--- frontend/src/components/Collage.js
+++ frontend/src/components/Collage.js
@@ -1,62 +1,68 @@
-import React from "react";
-import TitledImage from "./ui/TitledImage";
+import React from 'react';
+import TitledImage from './ui/TitledImage';
 
-import imgChiliPepper from "../assets/img/chili-pepper.png";
-import imgFermentedBeanPaste from "../assets/img/fermented-bean-paste.png";
-import imgSichuanPeppercorn from "../assets/img/sichuan-peppercorn.png";
-import imgFermentedBlackBeans from "../assets/img/fermented-black-beans.png";
-import imgGarlic from "../assets/img/garlic.png";
-import imgSpringOnion from "../assets/img/spring-onion.png";
+import imgChiliPepper from '../assets/img/chili-pepper.png';
+import imgFermentedBeanPaste from '../assets/img/fermented-bean-paste.png';
+import imgSichuanPeppercorn from '../assets/img/sichuan-peppercorn.png';
+import imgFermentedBlackBeans from '../assets/img/fermented-black-beans.png';
+import imgGarlic from '../assets/img/garlic.png';
+import imgSpringOnion from '../assets/img/spring-onion.png';
 
 const collageItems = [
   {
-    text: "chili pepper",
+    text: 'chili pepper',
     imgSrc: imgChiliPepper,
-    width: "49%"
+    width: '49%',
   },
   {
-    text: "fermented bean paste",
+    text: 'fermented bean paste',
     imgSrc: imgFermentedBeanPaste,
-    width: "49%"
+    width: '49%',
   },
   {
-    text: "sichuan peppercorn",
+    text: 'sichuan peppercorn',
     imgSrc: imgSichuanPeppercorn,
-    width: "36.5%"
+    width: '36.5%',
   },
   {
-    text: "fermented black beans",
+    text: 'fermented black beans',
     imgSrc: imgFermentedBlackBeans,
-    width: "23.8%"
+    width: '23.8%',
   },
   {
-    text: "garlic",
+    text: 'garlic',
     imgSrc: imgGarlic,
-    width: "11.1%"
+    width: '11.1%',
   },
   {
-    text: "spring onion",
+    text: 'spring onion',
     imgSrc: imgSpringOnion,
-    width: "23.4%"
-  }
+    width: '23.4%',
+  },
 ];
 
 const CollageElements = () => {
   return (
     <ul className="list-unstyled d-flex flex-wrap justify-content-between align-items-start collage__list">
-      {collageItems.map((item, index) => (
-        <TitledImage key={index} {...item} height={"50%"} />
-      ))}
+      {collageItems.map((item, index) => {
+        if (index == 1) {
+          return (
+            <div>
+              <TitledImage key={index} {...item} height={'50%'} />
+              <div className="w-100" style={{height: '10px'}} />
+            </div>
+          );
+        }
+
+        return <TitledImage key={index} {...item} height={'50%'} />;
+      })}
     </ul>
   );
 };
 
 const Collage = props => {
   return (
-    <div
-      className="w-75 h-70 mb-5 mx-auto collage"
-      style={{ maxWidth: "1440px" }}
-    >
+    <div className="w-75 mb-5 mx-auto collage" style={{maxWidth: '1440px'}}>
       <CollageElements />
     </div>
   );
--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -1,27 +1,29 @@
-import React from "react";
-import Logo from "../assets/mala-logo.svg";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Title from '../assets/img/home-logo.svg';
+import Logo from '../assets/mala-logo.svg';
+import {Link} from 'react-router-dom';
 
 const Header = props => (
   <div className="navbar navbar-expand-md header">
-    <div className="container-fluid">
+    <div className="container-fluid w-75 header__container">
       <div className="navbar-header header__title">
-        {/* <img className="header__title-icon" src={Logo} /> */}
-        <div className="header__title-text">mala sauce</div>
+        <img className="header__title-icon w-100" src={Title} />
       </div>
       <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link amiko" to={{ pathname: "/sauce" }}>
+        <li className="nav-item h-80">
+          <Link className="nav-link amiko black pb-0" to={{pathname: '/sauce'}}>
             The Sauce
           </Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link amiko" to={{ pathname: "/recipes" }}>
+        <li className="nav-item h-80">
+          <Link
+            className="nav-link amiko black pb-0"
+            to={{pathname: '/recipes'}}>
             Recipes
           </Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link amiko" to={{ pathname: "/news" }}>
+        <li className="nav-item h-80">
+          <Link className="nav-link amiko black pb-0" to={{pathname: '/news'}}>
             Newsletter
           </Link>
         </li>
@@ -32,7 +34,7 @@ const Header = props => (
 
 const Footer = () => {
   return (
-    <div className="w-100 position-relative" style={{ height: "200px" }}>
+    <div className="w-100 position-relative" style={{height: '200px'}}>
       <div className="d-flex justify-content-between position-absolute absolute-centered w-25">
         <a href="#">About Us</a>
         <img src={Logo} />
