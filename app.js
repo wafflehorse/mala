0cda3ec47501bd481f0f23822061dee6a43e0727
Parker Pestana <parker@everlane.com>
2018-12-04 16:11:44 -0800

    skeleton for email post

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const router = new Router();
 
 const app = new Koa();
+
+router.post('/email', () => {
+  console.log('you posted an emaillllll');
+});
+
+app.use(router.routes());
 
 app.listen(3000);
